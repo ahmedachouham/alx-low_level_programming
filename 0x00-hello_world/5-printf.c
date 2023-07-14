d3fdcb2b0253e96710abf88d705857e74ef73559
ahmedachouham <ahmedachouham@gmail.com>
2023-07-13 19:55:44 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * printf - function to print
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
