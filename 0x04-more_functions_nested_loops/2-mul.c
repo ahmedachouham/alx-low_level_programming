50b54f895929c1c8d6310f0850094929fb706689
ahmedachouham <ahmedachouham@gmail.com>
2023-07-21 14:38:48 -0700

    my first commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,14 @@
 include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
- * @a: a - Variable
- * @b: b - Variable
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-	int mult;
-
-	mult = a * b;
-
-	return (mult);
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
+    return (0);
 }
