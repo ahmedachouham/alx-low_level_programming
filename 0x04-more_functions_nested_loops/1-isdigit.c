b5ff4bf580d10ae01a4b608108051d7e1a9a5e27
ahmedachouham <ahmedachouham@gmail.com>
2023-07-21 14:26:28 -0700

    my first commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * _isdigit - A function that checks for uppercase character.
  * @c: An input character
@@ -6,5 +6,12 @@
  */
 int _isdigit(int c)
 {
-return (c > ='0' && c <= '9');
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
