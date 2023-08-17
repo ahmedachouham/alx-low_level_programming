1f90bbcdc7cb3bf5bf3607578a60736e481d377b
ahmedachouham <ahmedachouham@gmail.com>
2023-08-17 06:07:10 -0700

    my first commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,21 +1,24 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 /**
- * print_numbers - entry point
+ * print_strings - entry point
  * @separator: size of triangle
  * @n: size of triangle
  * Description: --
  * Return: --
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	int i = 0;
+	char *s;
 
 	va_start(args, n);
 	for (i = 0; i < (int)n; i++)
 	{
-		printf("%d%s", va_arg(args, int),
+		s = va_arg(args, char *);
+		printf("%s%s",
+			   s == NULL ? "(nil)" : s,
 			   i != (int)n - 1 && separator != NULL ? separator : "");
 	}
 	va_end(args);
